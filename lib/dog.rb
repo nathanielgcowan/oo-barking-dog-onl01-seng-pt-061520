7f47b12efe82a7a8c16f0f171c047230ac8adc65
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-04 22:17:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,1 @@
-
 class Dog
-
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-  
-  def name
-    @this_dogs_name
-  end
-  
-end
-
-class Dog
-  def bark
-    puts "woof!"
-  end
-end
